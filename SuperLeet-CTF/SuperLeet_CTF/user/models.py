0f4cd63cf909b01af439bfa8cc55bbf999af1d78
Ayush <patel.ayush08@gmail.com>
2017-06-18 16:38:19 +0530

    question model

--- SuperLeet-CTF/SuperLeet_CTF/user/models.py
+++ SuperLeet-CTF/SuperLeet_CTF/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
@@ -8,3 +9,12 @@ class User(models.Model):
     email=models.EmailField(max_length=256)
     password=models.CharField(max_length=32)
     admin=models.BooleanField(default=False)
+
+class Question(models.Model):
+
+    # def __init__(self, arg):
+    #     super(Question, self).__init__()
+    #     self.arg = arg
+
+    que=models.CharField(max_length=256)
+    options = JSONField()
